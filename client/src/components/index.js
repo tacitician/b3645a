55b5b74834cc813dc4227d99dff90373efde8655
Jeremiah <tacitician@pm.me>
2022-04-18 11:30:25 -0700

    updated logged out layout to match specs

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Home } from "./Home";
 export { default as SnackbarError } from "./SnackbarError";
-export { default as LoggedOutLayout, useStyles } from "./LoggedOutLayout";
+export { default as LoggedOutLayout } from "./LoggedOutLayout";
